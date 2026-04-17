8b7cc6a9a703a9a3340a22496dc6d3e00ee6ae26
Ahmed Sa <ahmed.alawlaqi25@gmail.com>
2026-04-17 21:26:17 +0300

    Done with Navbar-toggler

--- app/static/index.js
+++ app/static/index.js
@@ -1,7 +1,12 @@
 const btn = document.getElementById("menu-btn");
 const icon = btn.querySelector("i");
+const overlay = document.querySelector(".nav-overlay");
 
 btn.addEventListener("click", () => {
+    // toggle icon
     icon.classList.toggle("fa-bars");
     icon.classList.toggle("fa-xmark");
+
+    // toggle menu
+    overlay.classList.toggle("active");
 });
